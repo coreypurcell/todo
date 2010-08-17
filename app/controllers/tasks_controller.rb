b80341028b6d1f3ed3298083b03c7066bc3228ea
Corey Purcell <corey_purcell@pennunited.com>
2010-08-17 12:07:33 -0400

    edit a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,23 @@ class TasksController < ApplicationController
     respond_with( @task, :location => list_path(@list) )
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    if @task.save
+      flash[:notice] = "Task updated"
+    else
+      flash[:error] = "Could not update task"
+    end
+    respond_with( @task, :location => list_path(@list) )
+  end
+
   def complete
     @list = List.find(params[:list_id])
     @task = @list.tasks.find(params[:id])
